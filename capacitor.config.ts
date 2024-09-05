37b681dc5707506ebde5a56998901d35eb40c37e
Chen <saicharan.mupparam@eagles.oc.edu>
2024-09-05 17:30:59 +0000

    create ionic project;

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-Test-Play',
+  webDir: 'www'
+};
+
+export default config;
